403f25cdf8c49bed7ac8aacd28e02c45c7a3510a
sidorenkodm <88557275+sidorenkodm@users.noreply.github.com>
2021-11-06 14:20:18 -0700

    Update app.component.ts

--- angular-tree/src/app/app.component.ts
+++ angular-tree/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Node } from './tree components/node'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,23 +6,11 @@ import { Node } from './tree components/node'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  tree: Array<Node> = [new Node('root', this.generateTree(8, 5))];
 
-  generateTree (depth: number, maxWidth: number): Array<Node> {
-    if (depth === 0 ) {
-      return [];
-    }
+  onRenameDFS(){}
 
-    let result: Array<Node> = [];
+  onRenameBFS(){}
 
-    for (let i = 0; i < Math.floor(Math.random() * maxWidth + 1); i++){
-      let node = new Node ('Node ' + Math.floor(Math.random() * 999  + 1), this.generateTree(depth - 1 , maxWidth));
-      result.push(node);
-    }
-
-    return result;
-  }
 }
 
 
