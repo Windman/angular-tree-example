ca530d134afaffca352c034a93d3a259a208d6c7
sidorenkodm <88557275+sidorenkodm@users.noreply.github.com>
2021-11-07 13:14:50 -0800

    Update app.component.ts

--- angular-tree/src/app/app.component.ts
+++ angular-tree/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TreeGenerator } from './tree components/tree-generator';
+import { TreeNode } from './tree components/tree-node';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  treeGenerator: TreeGenerator = new TreeGenerator();
 
-  onRenameDFS(){}
+  @Input() tree: Array<TreeNode> = [new TreeNode('root', this.treeGenerator.generateTree(10, 8))];
 
-  onRenameBFS(){}
+  /*@Input() RenamedDFS = new EventEmitter();
+  @Input() RenamedBFS = new EventEmitter();
+  */
+  onRenameDFS() {
+    console.log("DFS");
+    /*let queue: Array<TreeNode> = [this.tree[0]];
+    let adj: Array<TreeNode> = [...this.tree[0].children];
 
+    while (adj.length >0){
+      for(let i = 0; i < adj.length; i++ ){
+        queue.push(adj[i]);
+      }
+    }
+    let currentNumber = 1;
+
+    while (){
+      queue.forEach(node => )
+    }*/
+  }
+  
+  onRenameBFS() {
+    console.log("BFS");
+  }
 }
 
 
