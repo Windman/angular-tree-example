56ef8c2c19375437df79906e31e6423477b8dff6
sidorenkodm <88557275+sidorenkodm@users.noreply.github.com>
2021-11-09 01:39:21 -0800

    Update app.module.ts

--- angular-tree/src/app/app.module.ts
+++ angular-tree/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Tree } from './tree components/tree.component';
+import { TreeComponent } from './tree components/tree.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Tree
+    TreeComponent,
   ],
   imports: [
     BrowserModule
