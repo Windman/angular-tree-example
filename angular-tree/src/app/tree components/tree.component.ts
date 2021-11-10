d23d412c094dc7b0a3f00c6870681cf3f1b44c26
sidorenkodm <88557275+sidorenkodm@users.noreply.github.com>
2021-11-10 01:31:01 -0800

    Update tree.component.ts

--- angular-tree/src/app/tree components/tree.component.ts
+++ angular-tree/src/app/tree components/tree.component.ts
@@ -1,6 +1,5 @@
-import {Component, HostListener, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { TreeNode } from './tree-node'
-import { TreeGenerator } from './tree-generator';
 
 @Component({
     selector: 'app-tree',
@@ -9,43 +8,7 @@ import { TreeGenerator } from './tree-generator';
 })
 
 export class TreeComponent {
-    //treeGenerator: TreeGenerator = new TreeGenerator();
+    
+    @Input() tree: Array<TreeNode>;
 
-    @Input() tree: Array<TreeNode>;// = [new TreeNode('root', this.treeGenerator.generateTree(10, 8))];
-
-    /*@HostListener('mouseenter') onMouseClick(event: Event) {
-        console.log(event);
-    }*/
-
-    /*@Input() treeRenameDFS(){
-        console.log('Renamed with DFS');
-    }
-
-    @Input() treeRenameBFS(){
-        console.log('Renamed with BFS');
-    }
-    /*
-    renameDFS(tree: Array<Node>, currentNumber: number) {
-      tree.forEach(node => {
-        if (node.data === 'root' && node.children.length !== 0) {
-          this.renameDFS(node.children, currentNumber)
-        } else {
-          node.data.replace(/[0-9]{3}/, currentNumber.toString());
-          if (node.children.length !== 0){
-            this.renameDFS(node.children, currentNumber + 1) 
-          }
-        }
-      });
-
-      //or use stack[].
-      //while (stack.length > 0){
-        check node
-        add all children to stack
-      }
-    }
-  
-    renameBFS(tree: Array<Node>, currentNumber: number){
-      //to do BFS
-    }
-    */
 }
